371766504220140a55eeb5a483a25cf3edcfccb9
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2022-05-26 13:15:41 +0530

    update

---
+++ config.dev.js
@@ -0,0 +1,47 @@
+const { dirname } = require('path');
+const path = require('path');
+module.exports={
+ mode:"development",
+ entry:'./src/index.js',
+ output: {
+   path: path.resolve(__dirname, 'dist'),
+   filename: 'output.js'
+ },
+   module: {
+     rules: [
+       { test: /\.css$/,
+        use:['style-loader','css-loader'] },
+
+        {
+         test: /\.(png|jpg|gif)$/,
+         use: {
+            loader: 'file-loader',
+            options: {
+               name: '[name].[ext]',
+               // name: '[path][name].[hash].[ext]',
+               outputPath: 'images'
+             }
+         }
+       },
+       {
+         test: /\.(html)$/,
+         use: {
+           loader: 'html-loader',
+           options: {
+             attrs: [':data-src']
+           }
+         }
+       }
+     ],
+   },
+ devServer: {
+   static: {
+     directory: path.join(__dirname, 'dist'),
+   },
+   compress: true,
+   port: 3200,
+   // devMiddleware: {
+   //    writeToDisk: true
+   //  }
+ }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import style from "./style.css"
+import image from "./image/example.jpg"
+console.warn(image);
+console.warn(style)
